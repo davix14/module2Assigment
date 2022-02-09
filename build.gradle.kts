545c30b026bfd08d6b50977045ef3c0fa8ddd045
divrex <jdavidjuarez95@gmail.com>
2022-02-08 22:00:27 -0700

    Start of poem reading

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jsoup:jsoup:1.11.3")
 }
 
 tasks.test {
--- src/main/kotlin/WordCounter.kt
+++ src/main/kotlin/WordCounter.kt
@@ -1,3 +1,4 @@
+import org.jsoup.Jsoup
 import java.net.URL
 import java.util.*
 import kotlin.collections.HashMap
@@ -14,35 +15,56 @@ class WordCounter constructor(
     var proceed = true
 
     object PoemConstants {
-        const val START_OF_POEM = "<div class=\"chapter\">"
-        const val END_OF_POEM = "</div><!--end"
+        const val START_OF_POEM = "by Edgar Allan Poe"
+        const val END_OF_POEM = "lifted—nevermore!"
     }
 
     fun getDocAndWords() {
         while (proceed) {
             var nextLine = scnr1.nextLine()
+            nextLine = Jsoup.parse(nextLine).text()
+//            println(nextLine)
+//            println(nextLine)
+            if (nextLine == PoemConstants.START_OF_POEM) {
+                println("START OF POEM")
+                while(proceed) {
+                    readPoem()
+                }
+            }
+
+/*
             if (nextLine == PoemConstants.START_OF_POEM) {
                 //  each loop will read the next line and parse the words from tha line
                 while (proceed) {
                     readPoem()
                 }
             }
+*/
         }
+        printWordCount()
     }
 
     private fun readPoem() {
-        scnr2 = Scanner(scnr1.nextLine())
+        scnr2 = Scanner(Jsoup.parse(scnr1.nextLine()).text())
         while (scnr2.hasNext()) {
             var nextWord = scnr2.next()
 
-            when(nextWord){
-
-            }
-
             if (nextWord == PoemConstants.END_OF_POEM) {
                 proceed = false
+                break
             }
+            println(nextWord)
+            addToWordCount(nextWord)
         }
+    }
+
+    private fun addToWordCount(wordIn: String){
+        val count = if(wordsMap.containsKey(wordIn)) wordsMap[wordIn] else 1
+        wordsMap[wordIn] = count!!
+    }
+
+    private fun printWordCount(){
+        wordsMap.toString()
     }
     //  START of poem <div class="chapter">
 // END of poem </div><!--end chapter-->
